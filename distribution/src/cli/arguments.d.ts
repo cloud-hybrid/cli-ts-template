2a1d170cea2f9fafc2a003e69b3b06d12b371245
Your Namegit <jacob.sanders@cloudhybrid.io>
2022-01-06 17:35:30 -0600

    [Development] - Ignore

--- distribution/src/cli/arguments.d.ts
+++
@@ -1,5 +0,0 @@
-import Arguments, { Argv } from "yargs";
-declare type Argument = Arguments.Argv;
-import { hideBin } from "yargs/helpers";
-export { Arguments, hideBin };
-export type { Argv, Argument };
