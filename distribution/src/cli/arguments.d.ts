0bf245b7e39e460ae4c635bc9d8eafe4eba0f66e
Your Namegit <jacob.sanders@cloudhybrid.io>
2022-01-06 17:34:48 -0600

    [Development] - Initialization

---
+++ distribution/src/cli/arguments.d.ts
@@ -0,0 +1,5 @@
+import Arguments, { Argv } from "yargs";
+declare type Argument = Arguments.Argv;
+import { hideBin } from "yargs/helpers";
+export { Arguments, hideBin };
+export type { Argv, Argument };
---
+++ distribution/src/cli/header.js
@@ -0,0 +1,16 @@
+import Colors from "chalk";
+const Year = new Date().getFullYear();
+const Title = Colors.bold.underline("CLI Application TS Template");
+const Copyright = Colors.dim(" - BSD-1 License" + "," + " " + Year);
+const Generate = () => {
+    const Lines = [];
+    /// Check if Local `run-script start` is interfacing `factory`
+    (process.env?.npm_lifecycle_event !== "start") && Lines.push("");
+    Lines.push(Title);
+    Lines.push(Copyright);
+    Lines.push("");
+    return Lines.join("\n");
+};
+const Header = Generate();
+export { Header };
+export default Header;
